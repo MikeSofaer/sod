f0f6a986e86006350ad98ddfc69aec0581bedfa7
Hari Manikarnika & Michael Sofaer <pair+hari+msofaer@pivotallabs.com>
2011-01-26 16:32:11 -0800

    Prepend our rubies to the path

--- lib/project_chef.rb
+++ lib/project_chef.rb
@@ -2,6 +2,11 @@ config = YAML.load(File.read('/etc/sod/Sodfile'))["production"]
 repo_name = config["repo"].split("/").last.gsub(".git","")
 $APP_PATH = File.join(config["app_dir"],repo_name)
 
+execute "prepend ruby executables to the path" do
+  command "echo 'export PATH=/usr/local/rvm/rubies/#{config["ruby_version"]}/bin:/usr/local/rvm/gems/#{config["ruby_version"]}/bin:$PATH' >> /etc/profile"
+  not_if {`echo $PATH`.split(":").select{|s| s.match /rubies/}.first == "/usr/local/rvm/rubies/#{config["ruby_version"]}/bin"}
+end
+
 include_recipe (config["cookbook"] + "::bootstrap")
 
 execute "Install bundler" do
