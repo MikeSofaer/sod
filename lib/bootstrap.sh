81a994caafa248881e678655b7cd275d615ba9ac
Michael Sofaer <msofaer@pivotallabs.com>
2011-11-15 15:00:33 -0800

    Back to ubuntu

--- lib/bootstrap.sh
+++ lib/bootstrap.sh
@@ -1,15 +1,14 @@
 #! /bin/bash
 set -ex
 # RVM dependencies
-#/usr/bin/aptitude install build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libxml2-dev libxslt-dev autoconf libc6-dev -y
-rpm -Uvh http://download.fedora.redhat.com/pub/epel/5/i386/epel-release-5-4.noarch.rpm || true
-/usr/bin/yum install gcc gcc-c++ kernel-devel patch make bison openssl libreadline6 libreadline6-devel curl git-core zlib1g zlib1g-devel libssl-devel libxml2-devel libxslt-devel autoconf libc6-devel -y
+apt-get install build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libxml2-dev libxslt-dev autoconf libc6-dev -y
+#rpm -Uvh http://download.fedora.redhat.com/pub/epel/5/i386/epel-release-5-4.noarch.rpm || true
+#/usr/bin/yum install gcc gcc-c++ kernel-devel patch make bison openssl libreadline6 libreadline6-devel curl git-core zlib1g zlib1g-devel libssl-devel libxml2-devel libxslt-devel autoconf libc6-devel -y
 
 /etc/sod/rvm_install.sh
 . /usr/local/lib/rvm >/dev/null
 rvm install $RUBY_VERSION
-
-export PATH=/usr/local/rvm/rubies/$RUBY_VERSION/bin:/usr/local/rvm/gems/$RUBY_VERSION/bin:$PATH
+rvm use $RUBY_VERSION
 
 gem install chef --no-rdoc --no-ri
 echo 'cookbook_path "/etc/sod/cookbooks"' > /etc/sod/solo.rb
