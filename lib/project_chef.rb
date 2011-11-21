674a830437897a23d20a4a8d856e9d54a1c4775d
Tray Torrance <devwork@warrentorrance.com>
2011-11-21 08:30:44 -0800

    Adds call to "sod::bootstrap" before "#{config['cookbook']}::default".

--- lib/project_chef.rb
+++ lib/project_chef.rb
@@ -2,6 +2,8 @@ config = YAML.load(File.read('/etc/sod/Sodfile'))["production"]
 repo_name = config["repo"].split("/").last.gsub(".git","")
 $APP_PATH = File.join(config["app_dir"],repo_name)
 
+include_recipe("sod::bootstrap")
+
 include_recipe (config["cookbook"] + "::default")
 
 if config["test_suite_recipe"]
